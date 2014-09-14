fa39944581e342659489b3b693fe937a7c34e8db
John Byrne <john.byrne.oz@gmail.com>
2014-09-14 23:57:24 +1000

    ViewModel loaded via ajax call. TaxCode modal display complete

--- ExpenseMeMVC/Controllers/HomeController.cs
+++ ExpenseMeMVC/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft;
 
 namespace ExpenseMeMVC.Controllers
 {
@@ -19,20 +20,47 @@ namespace ExpenseMeMVC.Controllers
 
         public ActionResult Index()
         {
-            var data = GetTransactionDetails();
+            return View(new ExpenseDetailsViewModel());
+        }
 
-            var tranDetails = data.First();
-            var lineItems = new List<LineItemDetails>();
+        [HttpGet]
+        public ActionResult GetExpenseDetailsViewModel()
+        {
+            var info = GetTransactionAndLineItemDetails("Visa0000000000006109");
 
-            var model = new ExpenseDetailsViewModel
+            var vm = new ExpenseDetailsViewModel();
+
+            vm.ExpenseTypeDetails = GetExpenseTypeDetals(info.First().CardType);
+            vm.TaxCodeDetails = GetTaxCodeDetals();
+            vm.GlCodeDetails = GetGlCodeDetals();
+
+            vm.Transaction = info.Select(x => new TransactionDetails
             {
-                TransactionDate = tranDetails.TransactionDate,
-                Amount = tranDetails.Amount,
-                MerchantName = tranDetails.MerchantName,
-                LineItems = lineItems
-            };
+                CardType = x.CardType,
+                ReferenceNumber = x.ReferenceNumber,
+                TransactionDate = x.TransactionDate.ToShortDateString(),
+                MerchantName = x.MerchantName,
+                Amount = x.Amount
+            }).First();
 
-            return View(model);
+            vm.LineItems = info.Select(x => new LineItemDetails
+            {
+                LineId = x.LineId,
+                Description = x.Description,
+                ExpenseType = x.ExpenseType,
+                TaxCode = x.TaxCode,
+                NetAmount = x.NetAmount,
+                TaxAmount = x.TaxAmount,
+                GrossAmount = x.GrossAmount,
+                Price = x.Price,
+                Quantity = x.Quantity,
+                CurrencyType = x.CurrencyType,
+                ExchangeRate = x.ExchangeRate,
+                GlCodes = ConvertGlCodesToList(x.GlCodes, vm.GlCodeDetails)
+            }).ToList();
+
+
+            return Json(vm, JsonRequestBehavior.AllowGet);
         }
 
         public ActionResult About()
@@ -49,15 +77,81 @@ namespace ExpenseMeMVC.Controllers
             return View();
         }
 
-        private List<TransactionDetails> GetTransactionDetails()
+        private List<GlCodeDetails> ConvertGlCodesToList(string glCodes, List<GlCodeDetails> codeDetails)
         {
-            var sql = @"select d.effective_transaction_date as TransactionDate, d.amount, d.merchant_name as MerchantName, 
-                               g.gl_code as GlCodes, g.tax_code as TaxCode, g.expense_type as ExpenseType
+            var codes = new List<GlCodeDetails>();
+            var glCodes2 = glCodes.Split('\t');
+
+            foreach (var item in codeDetails)
+            {
+                codes.Add(new GlCodeDetails
+                {
+                    SegmentId = item.SegmentId,
+                    Code = glCodes2[item.SegmentId - 1],
+                    Name = item.Name
+                });
+            };
+
+            return codes;
+        }
+
+        private List<ExpenseTypeDetails> GetExpenseTypeDetals(int cardType)
+        {
+            var sql = @"select expense_type as ExpenseType, description, gl_code as GlCodes from expense_types 
+                        where exists (select 1 from account_expense_types a where a.card_type = @0 and a.expense_type = expense_types. expense_type)";
+
+            return database.Fetch<ExpenseTypeDetails>(sql, cardType).ToList();
+        }
+
+        private List<TaxCodeDetails> GetTaxCodeDetals()
+        {
+            var sql = @"select tax_code as TaxCode, description, tax_percentage as TaxPercentage, tax_inclusive as TaxInclusive from tax_codes where card_type = 0";
+
+            return database.Fetch<TaxCodeDetails>(sql).ToList();
+        }
+
+        private List<GlCodeDetails> GetGlCodeDetals()
+        {
+            var sql = @"select segment_id as SegmentId, name as Name, char_limit as MaxLength, width as Width, validation_type as ValidationType 
+                          from gl_segment_defn where gl_type = (select gl_type from gl_type where status = 'active')";
+
+            return database.Fetch<GlCodeDetails>(sql).ToList();
+        }
+
+        private List<TransactionLineItemsQueryResult> GetTransactionAndLineItemDetails(string referenceNumber)
+        {
+            var sql = @"select d.card_type as CardType, d.reference_number as ReferenceNumber, 
+                                d.effective_transaction_date as TransactionDate, d.amount, d.merchant_name as MerchantName, 
+                                g.gl_code as GlCodes, g.tax_code as TaxCode, g.expense_type as ExpenseType,
+                                g.price, g.quantity, g.currency_type as CurrencyType, g.exchange_rate as ExchangeRate, 
+                                g.net_amount as NetAmount , g.tax_amount as TaxAmount, g.amount as GrossAmount, g.fbt_id as LineId
                         from statement_data d
                         inner join statement_gl_line g on g.card_type = d.card_type and g.reference_number = d.reference_number
                          where d.reference_number = @0";
 
-            return database.Fetch<TransactionDetails>(sql, "Visa0000000000006109").ToList();
+            return database.Fetch<TransactionLineItemsQueryResult>(sql, referenceNumber).ToList();
+        }
+
+        private class TransactionLineItemsQueryResult
+        {
+            public int CardType { get; set; }
+            public string ReferenceNumber { get; set; }
+            public DateTime TransactionDate { get; set; }
+            public decimal Amount { get; set; }
+            public string MerchantName { get; set; }
+            public int LineId { get; set; }
+            public string Description { get; set; }
+            public string ExpenseType { get; set; }
+            public string TaxCode { get; set; }
+            public decimal Price { get; set; }
+            public decimal Quantity { get; set; }
+            public string CurrencyType { get; set; }
+            public decimal ExchangeRate { get; set; }
+            public bool TaxReceipt { get; set; }
+            public decimal NetAmount { get; set; }
+            public decimal TaxAmount { get; set; }
+            public decimal GrossAmount { get; set; }
+            public string GlCodes { get; set; }
         }
     }
 }
--- ExpenseMeMVC/Models/ExpenseDetailsViewModel.cs
+++ ExpenseMeMVC/Models/ExpenseDetailsViewModel.cs
@@ -8,29 +8,60 @@ namespace ExpenseMeMVC.Models
 {
     public class ExpenseDetailsViewModel
     {
-        [DataType(DataType.Date)]
-        [Display(Name = "Transaction Date")]
-        public DateTime TransactionDate { get; set; }
-        public decimal Amount { get; set; }
-        [Display(Name = "Merchant Name")]
-        public string MerchantName { get; set; }
+        public TransactionDetails Transaction { get; set; }
         public List<LineItemDetails> LineItems { get; set; }
+        public List<ExpenseTypeDetails> ExpenseTypeDetails { get; set; }
+        public List<TaxCodeDetails> TaxCodeDetails { get; set; }
+        public List<GlCodeDetails> GlCodeDetails { get; set; }
     }
 
     public class TransactionDetails
     {
-        public DateTime TransactionDate { get; set; }
+        public int CardType { get; set; }
+        public string ReferenceNumber { get; set; }
+        public string TransactionDate { get; set; }
         public decimal Amount { get; set; }
         public string MerchantName { get; set; }
-        public string GlCodes { get; set; }
-        public string TaxCode { get; set; }
-        public string ExpenseType { get; set; }
     }
 
     public class LineItemDetails
     {
-        public string TaxCode { get; set; }
+        public int LineId { get; set; }
+        public string Description { get; set; }
         public string ExpenseType { get; set; }
-        public string GlCodes { get; set; }
+        public string TaxCode { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+        public string CurrencyType { get; set; }
+        public decimal ExchangeRate { get; set; }
+        public bool TaxReceipt { get; set; }
+        public decimal NetAmount { get; set; }
+        public decimal TaxAmount { get; set; }
+        public decimal GrossAmount { get; set; }
+        public List<GlCodeDetails> GlCodes { get; set; }
+    }
+
+    public class GlCodeDetails
+    {
+        public int SegmentId { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int MaxLength { get; set; }
+        public int Width { get; set; }
+        public string ValidationType { get; set; }
+    }
+
+    public class ExpenseTypeDetails
+    {
+        public string ExpenseType { get; set; }
+        public string Description { get; set; }
+        public string GlCode { get; set; }
+    }
+
+    public class TaxCodeDetails
+    {
+        public string TaxCode { get; set; }
+        public decimal TaxPercentage { get; set; }
+        public string TaxInclusive { get; set; }
     }
 }
